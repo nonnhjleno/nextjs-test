1b67cef70ae23e1f673ca413ae31a192f121c625
nonnhjleno <taaaisei999@gmail.com>
2024-02-18 00:13:32 +0900

    Linkでハイパーリンク作成

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Link from 'next/link'
 
 const about = () => {
   return (
-    <div>about</div>
+    <div className="flex flex-col items-center">
+      <Link href="/" className="underline">
+        Home
+      </Link>
+      <h1 className="text-2xl">About</h1>
+    </div>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
-import Image from "next/image";
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <h1>Hello Next.js</h1>
+    <div className="m-4">
+      <Link href="/about" className="underline">
+        About
+      </Link>
+      <h1 className="text-2xl">Home</h1>
+    </div>
   );
 }
