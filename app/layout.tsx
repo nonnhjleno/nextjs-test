bb9abe4ee82677311a64e47226cd5c18d98bb65f
nonnhjleno <taaaisei999@gmail.com>
2024-02-17 23:37:22 +0900

    layout.tsxを元に戻した

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`flex justify-center items-center h-screen`}>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
