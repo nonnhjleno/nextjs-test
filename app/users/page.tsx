0ea9d19f5f28fba4f2c28224134b71ea6a52c713
nonnhjleno <taaaisei999@gmail.com>
2024-02-19 00:10:26 +0900

    fetchでデータ取得

---
+++ app/users/page.tsx
@@ -0,0 +1,23 @@
+type User = {
+  id: string;
+  name: string;
+  email: string;
+};
+
+const Page = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const users: User[] = await response.json();
+  console.log(users);
+  return (
+    <div className="m-4">
+      <h1 className="text-lg font-bold">ユーザ一覧</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Page;
